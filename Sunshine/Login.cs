f23670eee260ac7a1192df58d750c1dd1ce38499
Isabeau de Waard <idewaard@live.nl>
2020-12-15 11:22:36 +0100

    Login.cs changed for database

--- Sunshine/Login.cs
+++ Sunshine/Login.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Sunshine
 {
     public partial class Login : Form
     {
+        connection con = new connection();
+        string id, username, password, firstname, lastname, address;
+
         public Login()
         {
             
@@ -20,21 +24,58 @@ namespace Sunshine
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-
-            User userLogin = new User();
-            
-            if (userLogin.LoginCheck(tbEmail.Text, tbPassword.Text, CreateAccount.accountEmail, CreateAccount.accountPassword))
+            try
             {
-                this.Hide();
-                Home form5 = new Home();
-                form5.Show();
-            }
-            else
-            {
-                MessageBox.Show("Wrong email or password. Please try again");
-            }
+                if (tbEmail.Text != "" && tbPassword.Text != "")
+                {
 
-        }
+                    con.Open();
+                    string query = "select id,username,password,firstname,lastname,address from user WHERE username ='" + tbEmail.Text + "' AND password ='" + tbPassword.Text + "'";
+                    MySqlDataReader row;
+                    row = con.ExecuteReader(query);
+                    if (row.HasRows)
+                    {
+                        while (row.Read())
+                        {
+                            id = row["id"].ToString();
+                            username = row["username"].ToString();
+                            password = row["password"].ToString();
+                            firstname = row["firstname"].ToString();
+                            lastname = row["lastname"].ToString();
+                            address = row["address"].ToString();
+                        }
+                        MessageBox.Show("Data found your name is " + firstname + " " + lastname + " " + " and your address at " + address);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not found", "Information");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is empty", "Information");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connection Error", "Information");
+            }
+        
+    
+            /* User userLogin = new User();
+
+             if (userLogin.LoginCheck(tbEmail.Text, tbPassword.Text, CreateAccount.accountEmail, CreateAccount.accountPassword))
+             {
+                 this.Hide();
+                 Home form5 = new Home();
+                 form5.Show();
+             }
+             else
+             {
+                 MessageBox.Show("Wrong email or password. Please try again");
+             }*/
+
+}
 
         private void btnNewAccount_Click(object sender, EventArgs e)
         {
