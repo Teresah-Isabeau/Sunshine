6715e0f299b5a129e96eceed5dbbd7bbb9ead17b
Isabeau de Waard <idewaard@live.nl>
2020-12-15 10:36:45 +0100

    Test

---
+++ Sunshine/Database.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Sunshine
+{
+    class Database
+    {
+        public static void Main()
+        {
+            string connStr = "server=192.168.172.20;user id=TestUser;database=Sunshine";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                // Perform database operations
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+
+
+    class connection
+    {
+
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        string myConnectionString;
+        static string host = "localhost";
+        static string database = "test";
+        static string userDB = "ecco";
+        static string password = "password";
+        public static string strProvider = "server=" + host + ";Database=" + database + ";User ID=" + userDB + ";Password=" + password;
+        public bool Open()
+        {
+            try
+            {
+                strProvider = "server=" + host + ";Database=" + database + ";User ID=" + userDB + ";Password=" + password;
+                conn = new MySqlConnection(strProvider);
+                conn.Open();
+                return true;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Connection Error ! " + er.Message, "Information");
+            }
+            return false;
+        }
+        public void Close()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+        public DataSet ExecuteDataSet(string sql)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+                da.Fill(ds, "result");
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+        public MySqlDataReader ExecuteReader(string sql)
+        {
+            try
+            {
+                MySqlDataReader reader;
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                reader = cmd.ExecuteReader();
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return null;
+        }
+        public int ExecuteNonQuery(string sql)
+        {
+            try
+            {
+                int affected;
+                MySqlTransaction mytransaction = conn.BeginTransaction();
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                affected = cmd.ExecuteNonQuery();
+                mytransaction.Commit();
+                return affected;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return -1;
+        }
+    }
+}
